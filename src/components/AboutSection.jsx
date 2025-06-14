215bac46ba9eac0d2f839773abb4f635630c1664
Walter <walter2000matsinhe@gmail.com>
2025-06-14 03:15:04 +0300

    first commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,86 @@
+import { Briefcase, Code, Download, User } from 'lucide-react'
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <section id='about' className='py-24 px-4 relative'>
+        {''}
+        <div className='container mx-auto max-w-5xl'>
+            <h2 className='text-space text-3xl md:text-4xl font-bold mb-12 text-center'>
+                About<span className='text-primary'> me</span>
+            </h2>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
+            <div className='space-y-6'>
+                <h3 className='text-2xl font-semibold'>Passionate web Developer and Tech Creator</h3>
+                <p className='text-muted-foreground '>
+                    With over 3 years of experience in web development , i socialize
+                    in creating responsive and accessible  web
+                    applications using modern technologies.
+                </p>
+                <p className='text-muted-foreground'>
+                    🎓 I'm a Computer Science student passionate about coding and technology.
+                    💻 I work with Java, CSS, React, HTML, and MySQL to build responsive, functional, and efficient applications.
+                    🌱 Currently, I'm diving deeper into full-stack development and enhancing my problem-solving skills.
+                    🚀 Let's connect and create something amazing together!
+                </p>
+                <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
+                    <a className='cosmic-button' href='#contact'>
+                        Get in Touch
+                    </a>
+                     <a className=' flex items-center gap-2 px-6 py-2 rounded-full border-primary text-primary hover:bg-primary/10 transition-colors duration-300' href='#contact'>
+                        <span className='underline'>Download CV</span><Download className=' h-6 w-6 text-primary'/>
+                    </a>
+                </div>
+            </div>
+
+            <div className='grid grid-cols-1 gap-6'>
+                <div className='gradient-border p-6 card-hover'>
+                    <div className='flex items-start gap-4'>
+                        <div className='p-3 rounded-full bg-primary/10'>
+                        <Code className='h-6 w-6 text-primary'/>
+                        </div>
+                        <div className='text-left'>
+                            <h4 className='font-semibold'>Web Develoment</h4>
+                            <p className='text-muted-foreground'>
+                                Creating Resonsive websites and web applications 
+                                with modern frameworks
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='gradient-border p-6 card-hover'>
+                    <div className='flex items-start gap-4'>
+                        <div className='p-3 rounded-full bg-primary/10'>
+                        <User className='h-6 w-6 text-primary'/>
+                        </div>
+                        <div className='text-left'>
+                            <h4 className='font-semibold'>UI/UX Design</h4>
+                            <p className='text-muted-foreground'>
+                               Designing intuitive user interfaces and seamless user exeriences
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='gradient-border p-6 card-hover'>
+                    <div className='flex items-start gap-4'>
+                        <div className='p-3 rounded-full bg-primary/10'>
+                        <Briefcase className='h-6 w-6 text-primary'/>
+                        </div>
+                        <div className='text-left'>
+                            <h4 className='font-semibold'>Project Managment</h4>
+                            <p className='text-muted-foreground'>
+                               Leading projects from conception to completion with agile methodologies.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,38 @@
+import { ArrowDown } from 'lucide-react';
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section
+      id='hero'
+      className='relative min-h-screen flex flex-col items-center justify-center px-4'
+    >
+      <div className='container max-w-4xl mx-auto text-center z-10'>
+        <div className='space-y-6'>
+          <h1 className='text-4xl md:text-6xl font-bold tracking-tight'>
+            <span className='opacity-0 animate-fade-in delay-[0ms]'>Hi, I'm </span>
+            <span className='opacity-0 text-primary animate-fade-in delay-[300ms]'>Walter</span>
+            <span className='opacity-0 text-gradient ml-2 animate-fade-in delay-[600ms]'>Matsinhe</span>
+          </h1>
+          <p className='text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto mt-10 mb-8 opacity-0 animate-fade-in-delay-3'>
+            🎓 I'm a Computer Science student passionate about coding and technology.
+            💻 I work with Java, CSS, React, HTML, and MySQL to build responsive, functional, and efficient applications.
+            🌱 Currently, I'm diving deeper into full-stack development and enhancing my problem-solving skills.
+            🚀 Let's connect and create something amazing together!
+          </p>
+          <div className='pt-4 opacity-0 animate-fade-in-delay-4'>
+            <a className='cosmic-button text-xl' href='#projects'>
+                View my Work
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'>
+      <span className='text-sm text-muted-foreground mb-2'>scroll</span>
+      <ArrowDown className='h-5 w-5 text-primary'/>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
